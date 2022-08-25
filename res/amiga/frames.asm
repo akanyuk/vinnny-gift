51f0358481e9991e10183811b342bb093623a256
marinov <marinov@sedmax.ru>
2022-08-25 17:45:52 +0300

    Amiga animation added

---
+++ res/amiga/frames.asm
@@ -0,0 +1,16 @@
+FRAMES
+	dw FRAME_0000
+	dw FRAME_0001
+	dw FRAME_0002
+	dw FRAME_0003
+	dw FRAME_0004
+	dw FRAME_0005
+	dw FRAME_0006
+FRAMES_END
+FRAME_0000	include "res/0000.asm"
+FRAME_0001	include "res/0001.asm"
+FRAME_0002	include "res/0002.asm"
+FRAME_0003	include "res/0003.asm"
+FRAME_0004	include "res/0004.asm"
+FRAME_0005	include "res/0005.asm"
+FRAME_0006	include "res/0006.asm"
--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,7 @@
 	device zxspectrum48
 
 
-	define _MUSIC_ 1
+	; define _MUSIC_ 1
 
 	org #6000
 page0s	di : ld sp, page0s
@@ -10,10 +10,8 @@ page0s	di : ld sp, page0s
 
 	ld a,#be, i,a, hl,interr, (#beff),hl : im 2 : ei
 
-	ld hl, SCR1
-	ld de, #4000
-	ld bc, #1b00
-	ldir
+	ld hl, POEM
+	call screenReplace
 
 	ifdef _MUSIC_
 	call PT3PLAY
@@ -45,14 +43,39 @@ page0s	di : ld sp, page0s
 
 	pop af : dec a : jr nz, .lineLoop
 
-	ld b, 100 : halt : djnz $-1
 
-	ld hl, SCR2
-	ld de, #4000
-	ld bc, #1b00
-	ldir
+	ld hl, amigaPlayer
+	call interrStart
+
+	ld b, 200 : halt : djnz $-1
+	ld b, 200 : halt : djnz $-1
+	ld b, 200 : halt : djnz $-1
+
+	call interrStop
+
+	ld hl, FACE
+	call screenReplace
 
 	jr $
+
+	; запуск нужной процедуры на прерываниях
+	; HL - адрес процедура
+interrStart	ld de, interrCurrent
+	ex de, hl
+	ld (hl), #c3 ; jp
+	inc hl : ld (hl), e
+	inc hl : ld (hl), d
+	ret
+
+	; остановка процедуры на прерываниях
+interrStop	ld hl, interrCurrent
+	ld (hl), #c9 ; ret
+	ret
+
+interrCurrent	ret
+	nop
+	nop
+
 
 interr	di
 	push af,bc,de,hl,ix,iy
@@ -63,14 +86,88 @@ interr	di
 	call PT3PLAY + 5	
 	endif
 
+	call interrCurrent
+
 	pop iy,ix,hl,de,bc,af
 	exx : ex af, af'
 	pop iy,ix,hl,de,bc,af
 	ei
 	ret
 
-SCR1	incbin "res/stih1 (white ink).scr"
-SCR2	incbin "res/sshot000000.scr"
+	; hl - screen
+screenReplace	ld (.movePixels+1), hl
+	ld de, #1800
+	add hl, de
+	ld (.moveAttrs+1), hl
+	
+	ld hl, #4000
+	ld (.movePixels+4), hl
+
+	ld hl, #5800
+	ld (.moveAttrs+4), hl
+
+	ld b,32
+.loop	push bc
+.movePixels	ld hl, #0000
+	ld de, #4000
+
+	ld a, 192
+	ld bc, #0020
+	push hl
+	push de
+1	push af
+	ld a,(hl) : ld(de),a
+	call DownDE
+	add hl,bc
+	pop af : dec a : jr nz,1b
+	pop de : inc de : ld (.movePixels+4),de
+	pop hl : inc hl : ld (.movePixels+1),hl	
+
+.moveAttrs	ld hl, #0000
+	ld de, #5800
+	
+	ld a,24
+	ld bc,#001f
+	push hl
+	push de	
+1	push af
+	ldi : inc bc
+	add hl,bc
+	ex de,hl : add hl,bc : ex de,hl
+	pop af : dec a : jr nz,1b
+	pop de : inc de : ld (.moveAttrs+4),de
+	pop hl : inc hl : ld (.moveAttrs+1),hl	
+
+	halt
+	pop bc
+	djnz .loop
+	ret
+
+DownDE	inc d : ld a,d : and #07 : ret nz : ld a,e : sub #e0 : ld e,a : sbc a,a : and #f8 : add a,d : ld d,a : ret
+
+amigaPlayer	ld a, 0 : inc a : and #03 : ld (amigaPlayer + 1), a
+	cp #01 : jr z, 1f
+	cp #03 : ret nz
+	ld de, #4000
+	call amiga1.DisplayFrame
+	jp amiga1.NextFrame
+1	ld de, #401a
+	call amiga2.DisplayFrame
+	jp amiga2.NextFrame
+	module amiga1
+	include "res/amiga/player.asm"
+	endmodule
+	module amiga2
+	include "res/amiga/player.asm"
+	endmodule
+	include "res/amiga/frames.asm"
+
+POEM	incbin "res/poem.bin"
+FACE	incbin "res/face.bin"
+
+	display $
+
+	org #c000
 PT3PLAY	include "src/PTxPlay.asm"
 	incbin "res/nq-underfiller-cut.pt3"
 
